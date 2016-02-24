1f09454ce4ee68992c6e93084b1930a087928c8d
quantjump <webcontact2@home-automation-community.com>
2016-02-24 18:09:58 +0100

    Fixes and installation files

--- test/write-csv.ts
+++ test/write-csv.ts
@@ -2,31 +2,18 @@
 /// <reference path="../typings/node/node.d.ts" />
 /// <reference path="../typings/lodash/lodash.d.ts" />
 /// <reference path="../typings/tape/tape.d.ts" />
-/// <reference path="../typings/sprintf-js/sprintf-js.d.ts" />
 
 "use strict";
 
-import * as path from "path";
+// To run this test in Visual Studio Code, go to View->Debug, then select the "Debug Current Unittest file" target. Then press F5.
 
+import * as path from "path";
 import * as test from "tape";
-import { sprintf } from "sprintf-js";
 
 import { processConfig, Config } from "./../src/config-helper";
 import * as fsp from "./../src/fs-promises";
 
 test("A passing test", (assert) => {
-    writeHeaderIfMissing1().then(() => {
-        assert.pass("This test will pass.");
-
-        assert.end();
-    }).catch((err) => {
-        console.log(err);
-    });
+    assert.pass("This test will pass.");
+    assert.end();
 });
-
-async function writeHeaderIfMissing1() {
-    const config = await processConfig("C:/Users/David/OneDrive/Documents/hardware-projects/code/dht2csv", "sensorconfig.json");
-    //await writeHeaderIfMissing2(config);
-}
-
-// 2015-01-01T00:00:00+01:00, -18.2
